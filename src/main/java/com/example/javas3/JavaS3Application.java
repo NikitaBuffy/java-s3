2ccf59adafb4c23145e6b881823affed9d7886a8
NikitaBuffy <greendickcreeper@yandex.ru>
2023-09-15 18:02:03 +0300

    initial commit

---
+++ src/main/java/com/example/javas3/JavaS3Application.java
@@ -0,0 +1,13 @@
+package com.example.javas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaS3Application.class, args);
+	}
+
+}
